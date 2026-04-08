2a010129d11e5583146c0692fe9ddfce27eb5ed6
Robert Garner <rgarner011235@gmail.com>
2026-04-08 13:52:14 -0600

    Add project files.

---
+++ DataDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataDemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DataDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
